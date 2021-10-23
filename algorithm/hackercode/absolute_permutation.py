13ebb4ba9cf99cce564129ee6411d40370153592
vail131 <vail131@naver.com>
2021-10-23 13:53:13 +0900

    hr

---
+++ algorithm/hackercode/absolute_permutation.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'absolutePermutation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#
+
+def absolutePermutation(n, k):
+    # Write your code here
+    if k==0:
+        return [i for i in range(1,n+1)]
+    elif n%(k*2)!=0:
+        return [-1]
+    
+    return [i+k if i%(k*2) in range(1,k+1) else i-k for i in range(1,n+1)]
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        result = absolutePermutation(n, k)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
